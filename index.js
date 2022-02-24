43e4c07819215bf90be98601510552a9c4746c3c
mousa haddadin <mousahaddadin@gmail.com>
2022-02-23 20:58:27 -0600

    added the generate html function in the index and added missing require package

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./employee");
 const Manager = require("./manager");
+const Engineer = require("./engineer");
 const Intern = require("./intern");
 
 // ask questions
-function askQuestions() {
-  const questions = [
+inquirer
+  .prompt([
     {
-      name: "name",
+      name: "role",
+      type: "list",
+      message: "please select role",
+      choices: ["manager", "engineer", "intern"],
+    },
+    {
+      name: "employeeName",
       type: "input",
       message: "enter employee name",
     },
@@ -39,5 +46,36 @@ function askQuestions() {
       type: "input",
       message: "enter interns school name",
     },
-  ];
-}
+  ])
+  .then((answers) => {
+    const theHTML = generateHTML(answers);
+    fs.writeFile("index.html", theHTML, (err) =>
+      err ? console.log(err) : console.log("success")
+    );
+  });
+
+const generateHTML = ({
+  role,
+  employeeName,
+  employeeId,
+  email,
+  officeNumber,
+  githubUsername,
+  school,
+}) => {
+  return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>`;
+};
